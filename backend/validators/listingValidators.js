728c0b3624d9a6892e57be622717483d84ba7f88
Ritik811 <ritikkumar32855@gmail.com>
2026-06-12 11:04:51 +0530

    Server side validation using zod

---
+++ backend/validators/listingValidators.js
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const listingValidationSchema = z.object({
+  title: z
+    .string({ required_error: "Property title is required" })
+    .trim()
+    .min(3, "Title required 3 characters"),
+
+  description: z
+    .string({ required_error: "Description is required" })
+    .trim()
+    .min(10, "description required 10 characters"),
+
+  category: z.enum(["Hotel", "Villa", "Resort", "Apartment"], {
+    errorMap: () => ({
+      message: "Category only Hotel, Villa, Resort ya Apartment ho sakti hai!",
+    }),
+  }),
+
+  price: z
+    .number({ required_error: "Price is required" })
+    .positive("Price always grater than 0"),
+
+  location: z.string({ required_error: "Location is required" }).trim(),
+  country: z.string({ required_error: "Country is required" }).trim(),
+  image: z.string().url("Image url is valid").optional().or(z.literal("")),
+});
